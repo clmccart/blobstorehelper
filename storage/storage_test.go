7f531de4a871186c980b9bef5774198a145d3fc4
Claire McCarthy <clmccart@microsoft.com>
2019-10-04 15:58:17 -0700

    first test

---
+++ storage/storage_test.go
@@ -0,0 +1,40 @@
+// run these tests by executing the following command from the command line:
+// go test ./storage/
+package storage
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"blobstorehelper/config"
+)
+
+var _ = Describe("Storage Account", func() {
+	// const timeout = time.Second * 180
+
+	BeforeEach(func() {
+		// TODO
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// TODO
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	Context("Create and Delete Storage Accounts", func() {
+
+		It("should create and delete storage account in azure", func() {
+			config.LoadSettings()
+			ctx := context.Background()
+		
+			storageAccountName := "clmteststorageaccount"
+			resourceGroup := "clm-go-dev"
+
+			_, err := CreateStorageAccount(ctx, storageAccountName, resourceGroup)
+
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
+
